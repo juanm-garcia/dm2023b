f9b13e2ea87d9234a91cfa4cdca8a021616fd04f
juanm-garcia <juanmgarcia@itba.edu.ar>
2023-10-04 15:22:10 -0300

    Experimento 01 Training Strategy MES TESTING

---
+++ src/workflow-inicial/601_RUN_correr_workflow_ExpB_01.r
@@ -0,0 +1,20 @@
+# Corrida general del workflow
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow
+
+# primeros pasos, relativamente rapidos
+source("~/dm2023b/src/workflow-inicial/611_CA_reparar_dataset_01.r")
+source("~/dm2023b/src/workflow-inicial/621_DR_corregir_drifting_01.r")
+source("~/dm2023b/src/workflow-inicial/631_FE_historia_ExpB_01.r")
+source("~/dm2023b/src/workflow-inicial/641_TS_training_strategy_ExpB_01.r")
+
+# ultimos pasos, muy lentos
+source("~/dm2023b/src/workflow-inicial/651_HT_lightgbm_ExpB_01.r")
+source("~/dm2023b/src/workflow-inicial/z661_ZZ_final_ExpB_01.r")
